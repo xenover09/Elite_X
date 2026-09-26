5b5ac5e4a55c6aef7693cf967d457f966e5d4c16
Salaar Hussain <xenoverhubofficial09@email.com>
2026-09-26 07:40:43 +0500

    Update bot status to show commands instead of server count

--- src/events/ready.js
+++ src/events/ready.js
@@ -18,7 +18,7 @@ module.exports = {
       client.user.setPresence({
         activities: [
           {
-            name: `${client.guilds.cache.size} servers | /dashboard`,
+            name: '/ask | /dashboard',
             type: ActivityType.Watching,
           },
         ],
